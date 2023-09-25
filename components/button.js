c8e075b5fce0916eb36f82a55733b811a7e34faa
jake085483 <jake085483@gmail.com>
2023-09-25 17:02:57 +0900

    start react

---
+++ components/button.js
@@ -0,0 +1,26 @@
+/* import React, {Component} from 'react';
+
+export default class Button extends Component{
+    constructor(){
+        super();
+        this.state={
+            mode:'info',
+            user:[
+                {id:'jake4321', password:'jake1234', name: 'Jake', address:'SiHeung', phone: '01012345678', email:'jake4321@naver.com', gender:'Male', age:'22' },
+                {id:'abc1234', password:'abc4321', name: 'Eom', address:'Yongin', phone: '01087654321', email:'abc1234@google.com', gender:'Male', age:'27' },
+                {id:'qwe5678', password:'qwe8765', name: 'Jane', address:'Ansan', phone: '01011112222', email:'qwe5678@daum.net', gender:'Female', age:'25' },
+                
+            ]
+           
+        }
+    }
+    getContents(){
+        let contents = [<Score mode={this.state.mode}
+                               name={this.state.name}
+                               students={this.state.students}>
+        </Score>];
+        if(this.state.mode==='info'){
+
+        }
+    }
+} */
